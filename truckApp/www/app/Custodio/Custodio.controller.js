0e737e6e7f28161e210cf5873df9b355e87b6dda
eldelasfranelas <juansimon18.js@gmail.com>
2016-12-21 01:01:54 -0400

    Custodio: 55

--- truckApp/www/app/Custodio/Custodio.controller.js
+++ truckApp/www/app/Custodio/Custodio.controller.js
@@ -4,18 +4,83 @@
   angular.module("truckApp.Custodio")
     .controller("CustodioCtrl", CustodioCtrl);
 
-  CustodioCtrl.$inject = ['$firebaseObject', '$scope', '$firebaseArray'];
+  CustodioCtrl.$inject = ['$firebaseObject', '$scope', '$firebaseArray', '$ionicModal'];
 
-  function CustodioCtrl($firebaseObject, $scope, $firebaseArray) {
+  function CustodioCtrl($firebaseObject, $scope, $firebaseArray, $ionicModal) {
     var vm = this;
+    vm.custodio = {};
 
-    var custodio = firebase.database().ref('custodio');
-    // var custodio = firebase.database().ref('custodio').child('0001');
-    // vm.dataCustodio = $firebaseObject(custodio);
+    //referencia a la coleccion custodio
+    const custodio = firebase.database().ref('custodio');
+
+    //ng-repeat
     vm.list = $firebaseArray(custodio);
-    // vm.dataCustodio.$bindTo($scope, 'custodio');
+
+    //modal para crear custodio
+    $ionicModal.fromTemplateUrl('custodioModal.html', {
+      id: 1,
+      scope: $scope,
+      animation: 'slide-in-up',
+      backdropClickToClose: true,
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      vm.modal = modal;
+    });
+
+    //modal para editar custodio
+    $ionicModal.fromTemplateUrl('abcd', {
+      id: 2,
+      scope: $scope,
+      animation: 'slide-in-up',
+      backdropClickToClose: true,
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      vm.modal2 = modal;
+    });
+
+    vm.abrirModal = function (a) {
+      if (a === 1) {
+        vm.modal.show();
+      } else {
+        vm.modal2.show();
+      }
+    };
+    vm.cerrarModal = function (a) {
+      if (a === 1) {
+        vm.modal.hide();
+      } else {
+        vm.modal2.hide()
+      }
+    };
+
+    //crear custodio
+    vm.registrarCustodio = function () {
+      custodio.child(vm.custodio.idempleado).set(vm.custodio);
+      vm.modal.remove();
+    }
+
+    //eliminar custodio
+    vm.eliminar = function (custodio) {
+      console.log(custodio);
+      vm.list.splice(vm.list.indexOf(custodio), 1);
+    };
+
+    //editar custodio
+    vm.editar = function (custodio) {
+      vm.modal2.show();
+      vm.custodio = custodio;
+    };
+
+    vm.guardar = function (a) {
+      debugger;
+      // vm.list.a
+    };
+
+    vm.mostrarBorrar = false;
+    vm.mostrarBorrarVarios = function () {
+      vm.mostrarBorrar = !vm.mostrarBorrar;
+    }
 
 
-    console.log(vm.list);
   }
 })();
