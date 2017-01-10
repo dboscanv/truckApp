7d938e4dcaa15f42025c09289fa94bf3670841ee
Diego <dboscan@firesoft.com.ve>
2017-01-10 00:34:35 -0400

    Fix bug

--- www/app/Camion/Camion.controller.js
+++ www/app/Camion/Camion.controller.js
@@ -4,9 +4,84 @@
   angular.module("truckApp.Camion")
     .controller("CamionCtrl", CamionCtrl);
 
-  CamionCtrl.$inject = ['checkAuth'];
+  CamionCtrl.$inject = ['checkAuth', "$firebaseArray", "$ionicModal", "$scope"];
 
-  function CamionCtrl(checkAuth) {
+  function CamionCtrl(checkAuth, $firebaseArray, $ionicModal, $scope) {
     var vm = this;
+    vm.camion = {};
+    const camion = firebase.database().ref("camion");
+
+    vm.eliminar = Eliminar;
+    vm.abrirModal = AbrirModal;
+    vm.cerrarModal = CerrarModal;
+    vm.guardarCamion = GuardarCamion;
+    vm.editar = Editar;
+
+    //Array list
+    vm.camiones = $firebaseArray(camion);
+    console.log(vm.camiones);
+
+    //Load modal's
+    $ionicModal.fromTemplateUrl("app/Camion/modales/crear_camion.html", {
+      scope: $scope,
+      animation: "slide-in-up",
+      backdropClickToClose: true,
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      vm.modal1 = modal;
+    });
+
+    $ionicModal.fromTemplateUrl("app/Camion/modales/editar_camion.html", {
+      scope: $scope,
+      animation: "slide-in-up",
+      backdropClickToClose: true,
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      vm.modal2 = modal;
+    });
+
+    function Eliminar(camion) {
+      vm.camiones.$remove(camion).then(function (ref) {
+        console.log(ref);
+        console.log(camion.$id == ref.key);
+      });
+    }
+
+    function AbrirModal(index, camion) {
+
+      if (index == 1) {
+        vm.modal1.show();
+      } else {
+        vm.modal2.show();
+        vm.camion = camion;
+      }
+    }
+
+    function CerrarModal(index) {
+      if (index == 1) {
+        vm.modal1.hide();
+      } else {
+        vm.modal2.hide();
+      }
+    }
+
+    function GuardarCamion() {
+      camion.child(vm.camion.id).set(vm.camion).then(function (ref) {
+        console.log(ref);
+        console.log("Añadido!");
+        vm.modal1.hide();
+      });
+    }
+
+    function Editar() {
+      vm.camiones.$save(vm.camion).then(function (ref) {
+        alert("Actualizado!");
+        vm.modal2.hide();
+      }, function (error) {
+        console.log(error);
+      });
+    }
+
+
   }
 })();
--- www/app/Clientes/Clientes.controller.js
+++ www/app/Clientes/Clientes.controller.js
@@ -4,10 +4,82 @@
   angular.module("truckApp.Clientes")
     .controller("ClientesCtrl", ClientesCtrl);
 
-  ClientesCtrl.$inject = ['checkAuth'];
+  ClientesCtrl.$inject = ['checkAuth', "$firebaseArray", "$ionicModal", "$scope"];
 
-  function ClientesCtrl(checkAuth) {
+  function ClientesCtrl(checkAuth, $firebaseArray, $ionicModal, $scope) {
+    
     var vm = this;
+    vm.cliente = {};
+    const cliente = firebase.database().ref("cliente");
+
+    vm.clientes = $firebaseArray(cliente);
+
+    vm.eliminar = Eliminar;
+    vm.abrirModal = AbrirModal;
+    vm.cerrarModal = CerrarModal;
+    vm.guardarCliente = GuardarCliente;
+    vm.editarCliente = Editar;
+
+    //Load modal's
+    $ionicModal.fromTemplateUrl("app/Clientes/modales/crear_cliente.html", {
+      scope: $scope,
+      animation: "slide-in-up",
+      backdropClickToClose: true,
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      vm.modal1 = modal;
+    });
+
+    $ionicModal.fromTemplateUrl("app/Clientes/modales/editar_cliente.html", {
+      scope: $scope,
+      animation: "slide-in-up",
+      backdropClickToClose: true,
+      hardwareBackButtonClose: true
+    }).then(function (modal) {
+      vm.modal2 = modal;
+    });
+
+    function Eliminar(cliente) {
+      vm.clientes.$remove(cliente).then(function (ref) {
+        console.log(ref);
+        console.log(cliente.$id == ref.key);
+      });
+    }
+
+    function AbrirModal(index, cliente) {
+      if (index == 1) {
+        vm.modal1.show();
+      } else {
+        vm.modal2.show();
+        vm.cliente = cliente;
+      }
+    }
+
+    function CerrarModal(index) {
+      if (index == 1) {
+        vm.modal1.hide();
+      } else {
+        vm.modal2.hide();
+      }
+    }
+
+    function GuardarCliente() {
+      cliente.child(vm.cliente.idcliente).set(vm.cliente).then(function (ref) {
+        console.log(ref);
+        console.log("Añadido!");
+        vm.modal1.hide();
+      });
+    }
+
+    function Editar() {
+      vm.clientes.$save(vm.cliente).then(function (ref) {
+        alert("Actualizado!");
+        vm.modal2.hide();
+      }, function (error) {
+        console.log(error);
+      });
+    }
+
 
   }
 })();
